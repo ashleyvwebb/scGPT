74826d71b70e30c6e309120e920f530626934e28
Ashley Webb <ashleywebb@Ashleys-MacBook-Air.local>
2025-12-11 10:55:51 +0000

    updated comments about masked_mse_loss

--- scgpt/loss.py
+++ scgpt/loss.py
@@ -2,6 +2,14 @@ import torch
 import torch.nn.functional as F
 
 
+ # ! Used to calulate the mean squared error 
+ # * MSE(x,y) = L = {l_1, l_2, ..., l_N} where l_i = (x_i - y_i)^2 and N is the batch size
+ # * when reduction='sum' we MSE(x,y) - sum(l_i) for i=1 to N 
+ # MSE is the same as squared L2 norm between each element in the input x and target y (x and y have the same shape)
+ # x and y are tensors of arbitrary shapes with a total of N elements each
+ # returns a tensor 
+ # ? A tensor is a multi-dimensional matrix containing elements of a single data type
+ # ? When a tensor is created with 'requires_grad=True', then torch.autograd records operations on them for automatic differentiation
 def masked_mse_loss( # TODO: What does this do?
     input: torch.Tensor, target: torch.Tensor, mask: torch.Tensor
 ) -> torch.Tensor:
