2bc93e78d19cfa503ac4b28123b6c8d0ad804fec
Ashley Webb <ashley.webb@warwick.ac.uk>
2026-04-01 23:05:06 +0100

    debugging

--- scgpt/research/experiments/common_prediction.py
+++ scgpt/research/experiments/common_prediction.py
@@ -59,7 +59,6 @@ def apply_mask_to_values(
     mask: np.ndarray,
     mask_token_value: int | float,
 ) -> np.ndarray:
-    print(values)
-    masked = values[0].toarray().flatten()
+    masked = values.copy()
     masked[mask] = mask_token_value
     return masked
