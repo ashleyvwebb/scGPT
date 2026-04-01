e7db24f014f0edd0e3748942a0ffe29007ece34f
Ashley Webb <ashley.webb@warwick.ac.uk>
2026-04-01 11:30:52 +0100

    debugging

--- scgpt/research/experiments/common_prediction.py
+++ scgpt/research/experiments/common_prediction.py
@@ -65,9 +65,9 @@ def apply_mask_to_values(
 
 
 def build_valid_mask(values: np.ndarray, pad_value: int | float | None = None) -> np.ndarray:
-    print(type(values.toarray()))
+    print(values)
     values = np.asarray(values)
-    print(type(values.toarray()))
+    print(values)
     # values = values[0]
     # print(type(values))
     if pad_value is None:
