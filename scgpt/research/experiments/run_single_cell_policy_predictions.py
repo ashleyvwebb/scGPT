cb7ae6ec6aa5fcb6ab6c888420240d066afedb82
Ashley Webb <ashley.webb@warwick.ac.uk>
2026-04-02 22:38:42 +0100

    redoing tests

--- scgpt/research/experiments/run_single_cell_policy_predictions.py
+++ scgpt/research/experiments/run_single_cell_policy_predictions.py
@@ -104,7 +104,7 @@ def preprocess_adata(
         filter_cell_by_counts=False,
         normalize_total=1e4,
         result_normed_key="X_normed",
-        log1p=False,
+        log1p=True,
         result_log1p_key="X_log1p",
         subset_hvg=False,
         hvg_flavor="seurat_v3",
@@ -270,7 +270,7 @@ def run_one_cell(
         pad_token="<pad>",
         pad_value=pad_value,
         append_cls=False,
-        include_zero_gene=True,
+        include_zero_gene=False,
     )
 
     tokenized_gene_ids = prepared["tokenized_gene_ids"]
