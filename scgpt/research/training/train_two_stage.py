d1090c75b63d81b13541a88669fda4c1d2a2e9a4
Ashley Webb <ashley.webb@warwick.ac.uk>
2026-04-04 14:30:32 +0100

    training

--- scgpt/research/training/train_two_stage.py
+++ scgpt/research/training/train_two_stage.py
@@ -139,7 +139,7 @@ def tokenize_dataset(adata, vocab):
     if hasattr(X, "toarray"):
         X = X.toarray()
 
-    genes = adata.var["gene_name"].tolist()
+    genes = adata.var["feature_name"].tolist()
 
     print("DATASET genes:", genes[:20])
 
