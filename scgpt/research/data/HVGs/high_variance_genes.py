5751dbddcbe82c47d12f08f2ed24b41844c750ad
Ashley Webb <ashley.webb@warwick.ac.uk>
2026-04-05 02:12:03 +0100

    HVG

--- scgpt/research/data/HVGs/high_variance_genes.py
+++ scgpt/research/data/HVGs/high_variance_genes.py
@@ -1,10 +1,42 @@
 import scanpy as sc
+import numpy as np
+import pandas as pd
 
 NUM_HVG = 50
+DATASET_PATH = "scgpt/research/data/dataset/train.h5ad"
+
+adata = sc.read_h5ad(DATASET_PATH)
+
+if "gene_name" not in adata.var.columns:
+    raise ValueError("Expected 'gene_name' column in adata.var")
+
+adata.var_names = adata.var["gene_name"].astype(str)
+
+adata.var_names_make_unique()
 
 sc.pp.highly_variable_genes(
     adata,
     flavor="seurat_v3",
     n_top_genes=NUM_HVG,
-    batch_key=None
+    batch_key="batch"
 )
+
+hvg_mask = adata.var["highly_variable"]
+hvg_genes = adata.var_names[hvg_mask]
+hvg_indicies = np.where(hvg_mask)[0]
+
+print(f"Number of HVGs: {len(hvg_genes)}")
+
+#TODO: finish this - need to complete the file path, as these may not be saved in the correct place
+# Save gene names
+pd.Series(hvg_genes).to_csv("scgpt/research/data/HVGs/hvg_genes.txt", index=False, header=False)
+
+# Save indicies
+np.save("scgpt/research/data/HVGs/hvg_indicies.npy", hvg_indicies)
+
+# Save annotated AnnData
+adata.write(DATASET_PATH)
+
+# Plot HVGs
+# TODO - look into saving this
+sc.pl.highly_variable_genes(adata, save="scgpt/research/data/HVGs/hvg_plot.png")
