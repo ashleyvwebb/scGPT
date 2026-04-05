82cee1d1d6ae287fc99fbeb97cbc487ba2df7084
Ashley Webb <ashley.webb@warwick.ac.uk>
2026-04-05 16:10:32 +0100

    HVG

--- scgpt/research/data/HVGs/high_variance_genes.py
+++ scgpt/research/data/HVGs/high_variance_genes.py
@@ -1,18 +1,11 @@
 import scanpy as sc
-import numpy as np
 import pandas as pd
+import numpy as np
 
 NUM_HVG = 50
 DATASET_PATH = "scgpt/research/data/dataset/train.h5ad"
 
 adata = sc.read_h5ad(DATASET_PATH)
-
-if "gene_name" not in adata.var.columns:
-    raise ValueError("Expected 'gene_name' column in adata.var")
-
-adata.var_names = adata.var["gene_name"].astype(str)
-
-adata.var_names_make_unique()
 
 sc.pp.highly_variable_genes(
     adata,
@@ -21,22 +14,36 @@ sc.pp.highly_variable_genes(
     batch_key="batch"
 )
 
-hvg_mask = adata.var["highly_variable"]
-hvg_genes = adata.var_names[hvg_mask]
-hvg_indicies = np.where(hvg_mask)[0]
+hvg_mask = adata.var["highly_variable"].values
+hvg_genes = adata.var.loc[hvg_mask, "feature_name"].astype(str)
 
-print(f"Number of HVGs: {len(hvg_genes)}")
-
-#TODO: finish this - need to complete the file path, as these may not be saved in the correct place
 # Save gene names
-pd.Series(hvg_genes).to_csv("scgpt/research/data/HVGs/hvg_genes.txt", index=False, header=False)
-
-# Save indicies
-np.save("scgpt/research/data/HVGs/hvg_indicies.npy", hvg_indicies)
-
-# Save annotated AnnData
-adata.write_h5ad(DATASET_PATH)
+hvg_genes.to_csv("scgpt/research/data/HVGs/train_hvg_genes.txt", index=False, header=False)
 
 # Plot HVGs
-# TODO - look into saving this
-sc.pl.highly_variable_genes(adata, save="scgpt/research/data/HVGs/hvg_plot.png")
+sc.pl.highly_variable_genes(adata, save="scgpt/research/data/HVGs/train_hvg_plot.png")
+
+
+def load_hvg_mask(adata, hvg_file):
+    """
+    Reconstruct HVG mask aligned with adata.X columns
+    
+    Parameters:
+        adata: AnnData
+        hvg_file: path to saved hvg_genes.txt
+
+    Returns:
+        hvg_mask: np.ndarray (bool)
+        hvg_indicies: np.ndarray (int)
+    """
+    hvg_genes = pd.reas_csv(hvg_file, header=None)[0].astype(str).values
+    
+    gene_names = adata.var["feature_name"].astype(str).values
+
+    hvg_set = set(hvg_genes)
+
+    hvg_mask = np.array([g in hvg_set for g in gene_names])
+
+    hvg_indicies = np.where(hvg_mask)[0]
+
+    return hvg_mask, hvg_indicies
